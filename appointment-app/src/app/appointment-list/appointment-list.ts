d4b26c82d57fcd2d20961e9efb184771e9837194
benshabbat <benshabbat27@gmail.com>
2025-09-11 12:59:37 +0300

    feat: implement appointment addition functionality with input fields

--- appointment-app/src/app/appointment-list/appointment-list.ts
+++ appointment-app/src/app/appointment-list/appointment-list.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-appointment-list',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './appointment-list.html',
   styleUrl: './appointment-list.css'
 })
 export class AppointmentList {
-  appointment:Appointment={
-    id:1,
-    title:'Dentist',
-    date:new Date(),
+  newAppointmentTitle: string = '';
+  newAppointmentDate: Date = new Date();
+  appointments: Appointment[] = [];
+
+  addAppointment() {
+    if (this.newAppointmentTitle && this.newAppointmentDate) {
+      const newAppointment: Appointment = {
+        id: this.appointments.length + 1,
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate
+      };
+      this.appointments.push(newAppointment);
+      this.newAppointmentTitle = '';
+      this.newAppointmentDate = new Date();
+    }
   }
 }
